5d7ea3708f77c3585936c6edaa83199f8c544b6c
Alex Andrade <g.alex.andrade@gmail.com>
2017-08-16 17:18:48 -0300

    Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from "@angular/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "app/app-routing.module";
@@ -14,6 +15,7 @@ import { CoreModule } from "app/core/core.module";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     AppRoutingModule,
     ShoppingModule,
